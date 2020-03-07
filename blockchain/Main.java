75449070785a82dbef64f2ff0fb328e2e4766b23
skrabby <ductruong1802@gmail.com>
2020-03-08 02:13:32 +0300

    BlockChain Essentials

--- blockchain/Main.java
+++ blockchain/Main.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<blockchain.Block> BCList = new ArrayList<blockchain.Block>();
+        List<Block> BCList = new ArrayList<Block>();
         String prevHash = "0";
         // Creating BlockChain with the size of 10
         for (int i = 0; i < 10; i++) {
-            String thisHash = new blockchain.StringUtil().applySha256(String.valueOf(i) + prevHash);
-            BCList.add(new blockchain.Block(new Date().getTime(), i, prevHash, thisHash));
+            String thisHash = new StringUtil().applySha256(String.valueOf(i) + prevHash);
+            BCList.add(new Block(new Date().getTime(), i, prevHash, thisHash));
             prevHash = thisHash;
         }
-        blockchain.BlockChain blocks = new blockchain.BlockChain(BCList);
+        BlockChain blocks = new BlockChain(BCList);
         // validation + printing 5 elements
         if (blocks.validateTheList())
             System.out.println(blocks.toNString(5));
