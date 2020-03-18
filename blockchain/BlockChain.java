60489c8e4f422974b3c4e27879ecb50b2c498dc5
skrabby <ductruong1802@gmail.com>
2020-03-18 21:08:17 +0300

    Complexity + Magic Number(Proof of work) + Serialization

--- blockchain/BlockChain.java
+++ blockchain/BlockChain.java
@@ -1,16 +1,46 @@
 package blockchain;
 
+import java.io.Serializable;
 import java.util.List;
 
-class Block {
+class BCValidation {
+    public static boolean tryHash(String hash, int nZero) {
+        for (int i = 0; i <= nZero; i++) {
+            if (i == nZero){
+                if (hash.charAt(i) == '0')
+                    return false;
+            }
+            else if (hash.charAt(i) != '0')
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean validateTheList(List<Block> BlockChain, int complexity) {
+        for (int i = 0; i < BlockChain.size() - 1; i++) {
+            if (!(BlockChain.get(i + 1).getHashOfPrevBlock().equals(BlockChain.get(i).getHashOfThisBlock())) ||
+                    !tryHash(BlockChain.get(i).getHashOfThisBlock(), complexity))
+                return false;
+        }
+        return !tryHash(BlockChain.get(BlockChain.size() - 1).getHashOfThisBlock(), complexity) ? false : true;
+    }
+}
+
+class Block implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private long timeStamp;
     private long id;
+    private long magicNumber;
+    private long creationTime;
     private String HashOfPrevBlock;
     private String HashOfThisBlock;
 
-    public Block(long timeStamp, long id, String hashOfPrevBlock, String hashOfThisBlock) {
+    public Block(long timeStamp, long id, String hashOfPrevBlock, String hashOfThisBlock, long magicNumber, long creationTime) {
         this.timeStamp = timeStamp;
         this.id = id;
+        this.magicNumber = magicNumber;
+        this.creationTime = creationTime;
         HashOfPrevBlock = hashOfPrevBlock;
         HashOfThisBlock = hashOfThisBlock;
     }
@@ -31,33 +61,40 @@ class Block {
         return HashOfThisBlock;
     }
 
+    public long getMagicNumber() { return magicNumber; }
+
+    public long getCreationTime() { return creationTime; }
+
     @Override
     public String toString() {
         return "Block:\n" +
                 "Id: " + id +
                 "\nTimestamp: " + timeStamp +
+                "\nMagic number: " + magicNumber +
                 "\nHash of the previous block:\n" + HashOfPrevBlock +
-                "\nHash of the block:\n" + HashOfThisBlock;
+                "\nHash of the block:\n" + HashOfThisBlock +
+                "\nBlock was generating for " + creationTime + " second" + (creationTime == 1 ? "" : "s");
     }
 }
 
-class BlockChain {
+class BlockChain implements Serializable {
+    private static final long serialVersionUID = 1L;
     private List<Block> BlockChain;
 
     public BlockChain(List<Block> blockChain) {
         BlockChain = blockChain;
     }
+
     public List<Block> getBlockChain() {
         return BlockChain;
     }
-    public boolean validateTheList() {
-        for (int i = 1; i < BlockChain.size() - 1; i++){
-            if (!(BlockChain.get(i + 1).getHashOfPrevBlock().equals(BlockChain.get(i).getHashOfThisBlock())))
-                return false;
-        }
-        return true;
-    }
 
+    public void setBlockChain(List<Block> blockChain) { BlockChain = blockChain; }
+
+    public void addBlock(Block block) {
+        if (block != null)
+            this.BlockChain.add(block);
+    }
     @Override
     public String toString() {
         String ret = "";
--- blockchain/Main.java
+++ blockchain/Main.java
@@ -1,22 +1,60 @@
 package blockchain;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
+    private static final String FILE_NAME = "BlockChain.data";
     public static void main(String[] args) {
-        List<Block> BCList = new ArrayList<Block>();
-        String prevHash = "0";
-        // Creating BlockChain with the size of 10
-        for (int i = 0; i < 10; i++) {
-            String thisHash = new StringUtil().applySha256(String.valueOf(i) + prevHash);
-            BCList.add(new Block(new Date().getTime(), i, prevHash, thisHash));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter how many zeros the hash has to start with: ");
+        int complexity = scanner.nextInt();
+        System.out.println();
+        BlockChain blockChain = null;
+        String prevHash;
+        int bcSize;
+        // Deserialization
+        try { blockChain = (BlockChain) SerializationUtils.deserialize(FILE_NAME); }
+        catch (FileNotFoundException fnf) {
+            blockChain = new BlockChain(new ArrayList<Block>());
+        }
+        catch (IOException | ClassNotFoundException e) { e.printStackTrace(); }
+        bcSize = blockChain.getBlockChain().size();
+        if (bcSize == 0)
+            prevHash = "0";
+        else
+            prevHash = blockChain.getBlockChain().get(bcSize - 1).getHashOfThisBlock();
+        // Creating/Adding Blocks (+3 blocks)
+        for (int i = bcSize; i < bcSize + 3; i++) {
+            long startTime = System.nanoTime();
+            String thisHash = "";
+            String hashToTry;
+            // Some Random Data
+            String blockData = String.valueOf(new Random().nextLong());
+            // Hash Brute-force
+            for (long magicNumber = 0; magicNumber < Long.MAX_VALUE; magicNumber++) {
+                hashToTry = blockData + prevHash + magicNumber;
+                thisHash = StringUtil.applySha256(hashToTry);
+                if (BCValidation.tryHash(thisHash, complexity)) {
+                    long endTime = System.nanoTime();
+                    blockChain.addBlock(new Block(new Date().getTime(), i, prevHash, thisHash, magicNumber, (endTime - startTime) / 1_000_000_000));
+                    break;
+                }
+            }
             prevHash = thisHash;
         }
-        BlockChain blocks = new BlockChain(BCList);
-        // validation + printing 5 elements
-        if (blocks.validateTheList())
-            System.out.println(blocks.toNString(5));
+        // Serialization
+        try { SerializationUtils.serialize(blockChain, FILE_NAME); }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Validation + printing
+        if (BCValidation.validateTheList(blockChain.getBlockChain(), complexity))
+            System.out.println(blockChain.toString());
         else
             System.out.println("BlockChain is corrupted!");
     }
